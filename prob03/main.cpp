a5fabda449f6076bbd10ee3cdc07ea9694eb2fc0
kbliaya <kbliaya@cs104u-6e.acad.fullerton.edu>
2019-09-21 14:13:46 -0700

    created a program to create rectangles with different bases.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,28 @@
+// Name: Kaylee Bliaya
 // Random Rectangle
+
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <cstdlib>
+
+int main ()
+{
+  //Set the maximum base of the rectangle to 40.
+  int max = 40;
+  int base;
+
+  //Generate random value for length of rectangle.
+  unsigned seed = time (0);
+  srand (seed);
+  base = rand() % max + 1;
+
+  //Print out the rectangle.
+  std::string rectangle;
+  rectangle.assign(base, '#');
+  std::cout << rectangle << '\n';
+  std::cout << rectangle << '\n';
+  std::cout << rectangle << '\n';
+
+  return 0;
+}
